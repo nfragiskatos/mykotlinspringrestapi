e4ac4056b29bd73f35aca4f95bfe375c4c8872fb
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-04-21 13:10:30 -0700

    Init Commit

---
+++ src/main/kotlin/com/nfragiskatos/mykotlinspringrestapi/MyKotlinSpringRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.nfragiskatos.mykotlinspringrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyKotlinSpringRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyKotlinSpringRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/nfragiskatos/mykotlinspringrestapi/MyKotlinSpringRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.mykotlinspringrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyKotlinSpringRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
